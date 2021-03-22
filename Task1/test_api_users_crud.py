624105044155b653ef4a7df5323ec110563e6d23
M Luhovyi <m.luhovoy@gmail.com>
2021-03-22 22:56:20 +0200

    minor changes

--- Task1/test_api_users_crud.py
+++ Task1/test_api_users_crud.py
@@ -1,4 +1,3 @@
-from urllib import response
 import requests
 import pytest
 
@@ -11,26 +10,21 @@ response_delete = requests.delete(f"{base_url}/public-api/users/123")
 
 
 def test_create():
-    # pass
     assert response_post.status_code == 201
 
 
 def test_read():
-    # print(response_get.json())
     assert response_get.status_code == 200
 
 
 def test_update():
-    # pass
     assert response_put.status_code != 304
 
 
 def test_modify():
-    # pass
-    assert response_patch.status_code == 304
+    assert response_patch.status_code == 200
 
 
 def test_delete():
-    # pass
     assert response_delete.status_code == 204
 
