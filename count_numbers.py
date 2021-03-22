ace17ae1727b397669af94a792ffc25013d6a894
M Luhovyi <m.luhovoy@gmail.com>
2021-03-22 20:57:36 +0200

    minor changes

--- count_numbers.py
+++ count_numbers.py
@@ -1,11 +1,9 @@
 def count_nums():
-    # file = open("number.txt", "r").read().splitlines()
     with open("number.txt", "r") as file:
         sum = 0.0
         for num in (line.strip() for line in file):
             if num:
                 if not num.startswith("#"):
-                    print(num)
                     sum += float(num)
     return sum
 
